a56b481f9a475d2f59afe0077a20807e854077d3
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-26 11:07:32 +0200

    showing online users in fe

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,18 +2,47 @@ import { useEffect, useState } from "react";
 
 export default function Chat() {
   const [ws, setWs] = useState(null);
+  const [onlinePeople, setOnlinePeople] = useState({});
+
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:9000");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
   }, []);
 
+  function showOnlinePeople(peopleArray) {
+    const people = {};
+    peopleArray.forEach(({ userId, username }) => {
+      people[userId] = username;
+    });
+    setOnlinePeople(people);
+  }
   function handleMessage(e) {
-    console.log("new message", e);
+    const messageData = JSON.parse(e.data);
+    if ("online" in messageData) {
+      showOnlinePeople(messageData.online);
+    }
   }
   return (
     <div className="flex h-screen">
-      <div className="bg-cyan-100 w-1/3">Contacts</div>
+      <div className="bg-cyan-100 w-1/3 pl-4 pt-4">
+        <div className="text-sky-500 font-bold flex gap-2 ">
+          <div>
+            {" "}
+            <img src="/logo.png" alt="logo" className="mx-auto h-12 w-12" />
+          </div>
+          <div className="items-center flex">
+            <p>Nimbus</p>
+          </div>
+        </div>
+
+        {Object.keys(onlinePeople).map((userId) => (
+          <div className="border-b border-gray-200 py-2">
+            {" "}
+            {onlinePeople[userId]}{" "}
+          </div>
+        ))}
+      </div>
       <div className="flex flex-col bg-sky-300 w-2/3 p-2">
         <div className="flex-grow"> messages with person</div>
         <div className="flex gap-2 ">
