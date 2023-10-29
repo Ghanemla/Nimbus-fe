d5760fa076435d5ac791454c68936e167a95dd05
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-29 09:20:33 +0100

    deploy test

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket("ws://localhost:9000");
+    const ws = new WebSocket("ws:https://nimbus-be.vercel.app/");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
